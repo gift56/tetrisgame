9c8b984a0c56cfc3901d27bc7b8afe94a1ebaf52
gift56 <efeasiughu@gmail.com>
2022-05-27 13:53:01 +0100

    get elements into our javascript

---
+++ styles/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector(".grid");
+    const scorceDisplay = document.querySelector('#score');
+    const startBtn = document.querySelector('#startBtn');
+    let square = Array.from(document.querySelectorAll('.grid div'));
+    const width = 10;
+});
